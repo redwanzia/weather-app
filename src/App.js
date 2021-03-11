46fe3df94a17431a0bdc4ddfc96ccdab28259fd0
redwan zia <redwan.zia@gmail.com>
2021-03-11 13:35:35 -0500

    added search bar and initial interface

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<main className='l'>
+				<div className='search-box'>
+					<input className='search-bar' placeholder='Search ...' type='text' />
+				</div>
+			</main>
+		</div>
+	);
+}
+
+export default App;
