41a57200082e0da751ebe68cecff357b6b0df25b
redwan zia <redwan.zia@gmail.com>
2021-03-11 13:58:50 -0500

    added place holders for the forcast with minor styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import './App.css';
 
 function App() {
+	const getDate = (d) => {
+		let months = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December'
+		];
+		let days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
+
+		let day = days[d.getDay()];
+		let date = d.getDate();
+		let month = months[d.getMonth()];
+		let year = d.getFullYear();
+
+		return `${day} ${date} ${month} ${year}`;
+	};
 	return (
 		<div className='App'>
 			<main className='l'>
 				<div className='search-box'>
 					<input className='search-bar' placeholder='Search ...' type='text' />
+				</div>
+				<div className='location-box'>
+					<div className='location'>Toronto</div>
+					<div className='date'>{getDate(new Date())}</div>
+				</div>
+				<div className='weather-box'>
+					<div className='temp'>15°c</div>
 				</div>
 			</main>
 		</div>
