9af7f6dc2e65b41d51922cb461ac7c9d240116c3
redwan zia <redwan.zia@gmail.com>
2021-03-11 17:40:15 -0500

    data fetching in the console has been completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,48 @@
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios'
 
 function App() {
+	const [ query, setQuery ] = useState('');
+	const [ weather, setWeather ] = useState();
+
+	console.log(query);
+	const temperature = 0;
+	const StringLocation = 'Toronto';
+
+  const stringUrl = 'https://fathomless-falls-90637.herokuapp.com/https://www.metaweather.com/api/location/search/?query=';
+  
+  const locationUrl = 'https://fathomless-falls-90637.herokuapp.com/https://www.metaweather.com/api/location/'
+
+	// 'https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/query=';
+
+	// const fetchSearch = (stringInput) => {
+	// 	let res = fetch(`${stringUrl}${query}`).then(res.json()).then((result) => {
+	// 		setWeather(result);
+	// 		setQuery('');
+	// 	});
+	// };
+	const fetchSearch = (e) => {
+		if (e.key === 'Enter') {
+      
+      axios.get(`${stringUrl}${query}`)
+      .then(response=>{
+        const result = response.data[0]
+        setWeather()
+        // console.log(result.woeid);
+            axios.get(`${locationUrl}${result.woeid}`)
+        .then(res=>{
+          const resultWoeid = res.data.consolidated_weather
+          setWeather(resultWoeid)
+          setQuery('')
+          console.log(weather);
+        })
+      })
+     
+      
+      
+		}
+	};
+
 	const getDate = (d) => {
 		let months = [
 			'January',
@@ -29,14 +71,28 @@ function App() {
 		<div className='App'>
 			<main className='l'>
 				<div className='search-box'>
-					<input className='search-bar' placeholder='Search ...' type='text' />
+					<input
+						className='search-bar'
+						placeholder='Search ...'
+						type='text'
+						onChange={(e) => setQuery(e.target.value)}
+						value={query}
+						onKeyPress={fetchSearch}
+					/>
 				</div>
 				<div className='location-box'>
-					<div className='location'>Toronto</div>
+					<div className='location'>{query}</div>
 					<div className='date'>{getDate(new Date())}</div>
 				</div>
 				<div className='weather-box'>
-					<div className='temp'>15°c</div>
+          <div className='t'>
+            {weather.map(el=>{
+              <ul>
+                <li>{el.the_temp}</li>
+              </ul>
+
+            })}
+          </div>
 				</div>
 			</main>
 		</div>
