8ca56f52d4f4cc89939afdc82e2fa3a8bb406e68
redwan zia <redwan.zia@gmail.com>
2021-03-11 20:29:39 -0500

    minor CSS adjustment

--- src/App.js
+++ src/App.js
@@ -1,75 +1,32 @@
 import React, { useState } from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 function App() {
 	const [ query, setQuery ] = useState('');
-  const [ weather, setWeather ] = useState([]);
-  const [title, setTitle] = useState('')
+	const [ weather, setWeather ] = useState([]);
+	const [ title, setTitle ] = useState('');
 
-	
+	const stringUrl =
+		'https://fathomless-falls-90637.herokuapp.com/https://www.metaweather.com/api/location/search/?query=';
 
-  const stringUrl = 'https://fathomless-falls-90637.herokuapp.com/https://www.metaweather.com/api/location/search/?query=';
-  
-  const locationUrl = 'https://fathomless-falls-90637.herokuapp.com/https://www.metaweather.com/api/location/'
+	const locationUrl = 'https://fathomless-falls-90637.herokuapp.com/https://www.metaweather.com/api/location/';
 
-	// 'https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/query=';
-
-	// const fetchSearch = (stringInput) => {
-	// 	let res = fetch(`${stringUrl}${query}`).then(res.json()).then((result) => {
-	// 		setWeather(result);
-	// 		setQuery('');
-	// 	});
-	// };
 	const fetchSearch = (e) => {
 		if (e.key === 'Enter') {
-      
-      axios.get(`${stringUrl}${query}`)
-      .then(response=>{
-        const result = response.data[0]
-        setTitle(result.title)       
-        
-     
-        axios.get(`${locationUrl}${result.woeid}`)
-        .then(res=>{
-          const resultWoeid = res.data.consolidated_weather
-          setWeather(resultWoeid)
-          setQuery('')
-          console.log(weather);
-        })
-      })
-     
-      
-      
+			axios.get(`${stringUrl}${query}`).then((response) => {
+				const result = response.data[0];
+				setTitle(result.title);
+
+				axios.get(`${locationUrl}${result.woeid}`).then((res) => {
+					const resultWoeid = res.data.consolidated_weather;
+					setWeather(resultWoeid);
+					setQuery('');
+					console.log(weather);
+				});
+			});
 		}
-  };
-  
-
- 
-
-	const getDate = (d) => {
-		let months = [
-			'January',
-			'February',
-			'March',
-			'April',
-			'May',
-			'June',
-			'July',
-			'August',
-			'September',
-			'October',
-			'November',
-			'December'
-		];
-		let days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
-
-		let day = days[d.getDay()];
-		let date = d.getDate();
-		let month = months[d.getMonth()];
-		let year = d.getFullYear();
-
-		return `${day} ${date} ${month} ${year}`;
 	};
+
 	return (
 		<div className='App'>
 			<main className='l'>
@@ -85,28 +42,29 @@ function App() {
 				</div>
 				<div className='location-box'>
 					<div className='location'>{title}</div>
-					<div className='date'>{getDate(new Date())}</div>
 				</div>
 				<div className='weather-box'>
-          <div className='t'>
-           <h1>Five day Forecast</h1>
-            {weather.map(el=>{
-                return(
-                  <div className='card'>
-                   
-                    <h2> Day:{el.applicable_date}</h2>
-                    <h2> Temperature :{el.the_temp}</h2>
-                    <h2> Minimum Temperature :{el.min_temp}</h2>
-                    <h2> Maximum Temperature :{el.max_temp}</h2>                  
-                  </div>
-                ) 
-              })}
-          </div>
+					<div className='t'>
+						<h1>Five day Forecast</h1>
+						{weather.map((el) => {
+							return (
+								<div className='container'>
+									<div className='card'>
+										<h2> Day:{el.applicable_date}</h2>
+										<h2> {el.weather_state_name}</h2>
+										<h3> Temperature : {Math.round(el.the_temp)}°C</h3>
+										<h3> Minimum Temperature : {Math.round(el.min_temp)}°C</h3>
+										<h3> Maximum Temperature : {Math.round(el.max_temp)}°C</h3>
+										<h3> Humidity : {el.humidity}</h3>
+									</div>
+								</div>
+							);
+						})}
+					</div>
 				</div>
 			</main>
 		</div>
 	);
 }
-
 
 export default App;
--- src/App1.js
+++ src/App1.js
@@ -1,24 +1,48 @@
-
 import './App.css';
 
 function App() {
-  const rooms = [
-    { room_type: "Queen", vacant_rooms: 5, price: 100 },
-    { room_type: "Double", vacant_rooms: 3, price: 75 },
-    { room_type: "Twin", vacant_rooms: 8, price: 60 }
-  ];
+	const rooms = [
+		{ room_type: 'Queen', vacant_rooms: 5, price: 100 },
+		{ room_type: 'Double', vacant_rooms: 3, price: 75 },
+		{ room_type: 'Twin', vacant_rooms: 8, price: 60 }
+	];
 
-  return (
-    <div className="App">
-    {rooms.map(el=>(
-      <ol>
-        <li>{el.room_type}</li>
-        <li>{el.vacant_rooms}</li>
-        <li>{el.price}</li>
-      </ol>
-    ))}
-    </div>
-  );
+	return (
+		<div className='App'>
+			{rooms.map((el) => (
+				<ol>
+					<li>{el.room_type}</li>
+					<li>{el.vacant_rooms}</li>
+					<li>{el.price}</li>
+				</ol>
+			))}
+		</div>
+	);
 }
 
 export default App;
+
+const getDate = (d) => {
+	let months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December'
+	];
+	let days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ];
+
+	let day = days[d.getDay()];
+	let date = d.getDate();
+	let month = months[d.getMonth()];
+	let year = d.getFullYear();
+
+	return `${day} ${date} ${month} ${year}`;
+};
